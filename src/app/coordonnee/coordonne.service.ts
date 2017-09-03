8690846fa64c587fe7f4704e8dfac650abee9cac
achraf <achraf_bkr@hotmail.com>
2017-09-03 18:54:21 +0200

    Commit tslint.

--- src/app/coordonnee/coordonne.service.ts
+++ src/app/coordonnee/coordonne.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CoordonneeModel } from "./coordonneeModel";
+import { CoordonneeModel } from './coordonneeModel';
 
 @Injectable()
 export class CoordonneService {
