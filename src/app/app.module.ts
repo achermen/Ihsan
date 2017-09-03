966ab854f96c5ec30df79e261d9e19f2af2cdad3
Ahmed <ahmedd.trabelsiigmail.com>
2017-09-03 19:06:53 +0200

    Authentication Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 
 import { AppComponent} from './app.component';
 import { BeneficiaireComponent } from './beneficiaire/beneficiaire.component';
 import { AidesComponent } from './aides/aides.component';
 import { RouterModule} from '@angular/router';
-import { BeneficiaireListComponent } from './Beneficiaire-list/Beneficiaire-list.component'
+import { BeneficiaireListComponent } from './Beneficiaire-list/Beneficiaire-list.component';
 
 // Service.
-import { BeneficiaireService } from './beneficiaire.service'
+import { BeneficiaireService } from './beneficiaire.service';
 import {CoordonneService} from './coordonnee/coordonne.service';
 import {LogementService} from './logement/logement.service';
 import {SanteService} from './sante/sante.service';
@@ -26,24 +28,21 @@ import { CoordonneeComponent } from './coordonnee/coordonnee.component';
 import { LogementComponent } from './logement/logement.component';
 import { SanteComponent } from './sante/sante.component';
 import { RessourceComponent } from './ressource/ressource.component';
- 
 @NgModule({
   declarations: [
-    AppComponent,      
-    BeneficiaireComponent,  
+    AppComponent,
+    BeneficiaireComponent,
     AidesComponent,
-    BeneficiaireListComponent
-    
-      , DataTableDemo1Remote
-      ,DataTableDemo1
-      ,DataTableDemo2
-      , DataTableDemo3, CoordonneeComponent, LogementComponent, SanteComponent, RessourceComponent 
-
-  ],
+    BeneficiaireListComponent,
+    DataTableDemo1Remote,
+    DataTableDemo1,
+    DataTableDemo2,
+    DataTableDemo3, CoordonneeComponent, LogementComponent, SanteComponent, RessourceComponent],
   imports: [
-  DataTableModule,
+    DataTableModule,
     FormsModule,
     BrowserModule,
+    NgbModule,
     RouterModule.forRoot([
        {
         path: 'DataTable1',
@@ -61,17 +60,17 @@ import { RessourceComponent } from './ressource/ressource.component';
         path: 'beneficiaireList',
         component: BeneficiaireListComponent
       },
-	   {		
-	   		path: 'beneficiaire/:numeroDossier',
-	   		component:BeneficiaireComponent
-	   	},
-	   	{
-	   	 path: 'aides',
-	   	 component: AidesComponent	
-
-	   	}])
+      {
+        path: 'beneficiaire/:numeroDossier',
+        component: BeneficiaireComponent
+      },
+      {
+        path: 'aides',
+        component: AidesComponent
+      }
+    ])
   ],
-  providers: [BeneficiaireService,CoordonneService,LogementService,SanteService,RessourceService],
+  providers: [BeneficiaireService, CoordonneService, LogementService, SanteService, RessourceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
