1c90e665c94b36599c37f6cfd785f4f6908159ab
achraf <achraf_bkr@hotmail.com>
2017-09-10 22:21:27 +0200

    Ajout beneficiaire Model

--- src/app/beneficiaire.service.ts
+++ src/app/beneficiaire.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BeneficiaireModel } from './beneficiaire-list/beneficiaireModel';
+import { BeneficiaireModel } from './beneficiaire/beneficiaireModel';
 
 
 @Injectable()
